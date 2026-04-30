090b22a42f605b00de289f3dd85dc9bce690fd10
Rahul Chaubey <81133483+Ryan1013@users.noreply.github.com>
2026-04-30 16:42:47 +0100

    Add files via upload

--- batter_dashboard.py
+++ batter_dashboard.py
@@ -299,7 +299,7 @@ st.sidebar.header("Filters")
 
 teams = sorted(data['Batting Team'].dropna().unique())
 
-default_team = "Glamorgan Women" if "Glamorgan Women" in teams else teams[0]
+default_team = "Gloucestershire Women" if "Gloucestershire Women" in teams else teams[0]
 
 selected_teams = st.sidebar.multiselect(
     "Batting Team",
